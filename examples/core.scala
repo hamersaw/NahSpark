2e0c10b7e4643315d2d8d4a74bb44cd658821044
Daniel Rammer <hamersaw@bushpath.com>
2019-07-23 15:03:26 -0600

    added a small scala example

---
+++ examples/core.scala
@@ -0,0 +1,17 @@
+// register AtlasGeometryUDT and User Defined Functions
+import org.apache.spark.sql.atlas.AtlasRegister
+AtlasRegister.init(spark)
+
+// read dataframe from atlas csv file
+import org.apache.spark.sql.atlas.datasources.AtlasRelation
+val df = spark.sqlContext.read.format("atlas").load("hdfs://127.0.0.1:9000/user/hamersaw")
+//val df = spark.sqlContext.read.format("csv").load("hdfs://127.0.0.1:9000/user/hamersaw")
+
+df.printSchema()
+
+// parase AtlasGeometryUDT
+//df.createOrReplaceTempView("atlas_test")
+//var spatialDf = spark.sql("SELECT BuildPoint(_c0, _c1) AS point, _c2, _c3 FROM atlas_test")
+
+//spatialDf.createOrReplaceTempView("spatial_test")
+//var distanceDf = spark.sql("SELECT point, Distance(point, BuildPoint(0.0, 10.0)) as distance, _c2, _c3 FROM spatial_test")
