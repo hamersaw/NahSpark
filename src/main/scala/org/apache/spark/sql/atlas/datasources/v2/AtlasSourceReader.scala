9018e7b608f576118d541b5d357feefe0708129a
Daniel Rammer <hamersaw@bushpath.com>
2019-08-16 10:54:07 -0600

    multithreaded atlas source reader.

--- src/main/scala/org/apache/spark/sql/atlas/datasources/v2/AtlasSourceReader.scala
+++ src/main/scala/org/apache/spark/sql/atlas/datasources/v2/AtlasSourceReader.scala
@@ -11,7 +11,9 @@ import org.apache.spark.sql.sources.Filter
 import org.apache.spark.sql.sources.v2.reader.{DataSourceReader, InputPartition, SupportsPushDownFilters, SupportsPushDownRequiredColumns}
 import org.apache.spark.sql.types.{LongType, StringType, StructType}
 
+import java.lang.Thread
 import java.util.{ArrayList, HashSet, List}
+import java.util.concurrent.ArrayBlockingQueue
 
 import scala.collection.JavaConversions._
 import scala.collection.mutable.ListBuffer
@@ -55,11 +57,111 @@ class AtlasSourceReader(fileMap: Map[String, Seq[FileStatus]],
 
     val atlasQuery = atlasQueryExpressions.mkString("&")
 
+    // TODO - submit requests within threads
+    
+
+    val threadCount = 4
+    val inputQueue = new ArrayBlockingQueue[(String, Int, String, Long)](4096)
+    val outputQueue = new ArrayBlockingQueue[Any](128)
+
+    val threads: List[Thread] = new ArrayList()
+    for (i <- 0 to threadCount) {
+      val thread = new Thread() {
+        override def run() {
+          breakable { while (true) {
+            // retrieve next file
+            val (ipAddress, port, filename, length) = inputQueue.take()
+            if (filename.isEmpty) {
+              break
+            }
+
+            // send GetFileInfo request
+            val gblRpcClient = new RpcClient(ipAddress, port, "ATLAS-SPARK",
+              "org.apache.hadoop.hdfs.protocol.ClientProtocol")
+            val gblRequest = ClientNamenodeProtocolProtos
+              .GetBlockLocationsRequestProto.newBuilder()
+                .setSrc(filename)
+                .setOffset(0)
+                .setLength(length)
+                .build
+
+            val gblIn = gblRpcClient.send("getBlockLocations", gblRequest)
+            val gblResponse = ClientNamenodeProtocolProtos
+              .GetBlockLocationsResponseProto.parseDelimitedFrom(gblIn)
+            gblIn.close
+            gblRpcClient.close
+
+            // process block locations
+            val lbsProto = gblResponse.getLocations
+            outputQueue.put(lbsProto)
+          } }
+
+          outputQueue.put("")
+        }
+      }
+
+      thread.start()
+      threads.append(thread)
+    }
+
+    // push paths down pipeline
+    for ((id, files) <- this.fileMap) {
+      // parse ipAddress, port
+      val idFields = id.split(":")
+      val (ipAddress, port) = (idFields(0), idFields(1).toInt)
+
+      for (file <- files) {
+        var path = file.getPath.toString
+        if (!atlasQuery.isEmpty) {
+          path += ("+" + atlasQuery)
+        }
+
+        inputQueue.put((ipAddress, port, path, file.getLen))
+      }
+    }
+
+    for (i <- 0 to threadCount) {
+      inputQueue.put(("", 0, "", 0))
+    }
+
     // retrieve block list
     var hosts: Map[String, HashSet[Long]] = Map()
     var blocks: Map[Long, HdfsProtos.LocatedBlockProto] = Map()
 
-    for ((id, files) <- this.fileMap) {
+    var poisonCount = 0
+    while (poisonCount < threadCount) {
+      val result = outputQueue.take()
+      if (!result.isInstanceOf[HdfsProtos.LocatedBlocksProto]) {
+        poisonCount += 1
+      } else {
+        val lbsProto = result.asInstanceOf[HdfsProtos.LocatedBlocksProto]
+
+        for (lbProto <- lbsProto.getBlocksList) {
+          // parse block id
+          val blockId = lbProto.getB.getBlockId
+          blocks += (blockId -> lbProto)
+
+          // parse locations
+          for (diProto <- lbProto.getLocsList) {
+            val didProto = diProto.getId
+            val address = didProto.getIpAddr + ":" + didProto.getXferPort
+
+            val set = hosts.get(address) match {
+              case Some(set) => set
+              case None => {
+                val set: HashSet[Long] = new HashSet
+                hosts += (address -> set)
+                set
+              }
+            }
+
+            set.add(blockId)
+          }
+        }
+      }
+    }
+
+    /*for ((id, files) <- this.fileMap) {
       // parse ipAddress, port
       val idFields = id.split(":")
       val (ipAddress, port) = (idFields(0), idFields(1).toInt)
@@ -112,7 +214,7 @@ class AtlasSourceReader(fileMap: Map[String, Seq[FileStatus]],
           }
         }
       }
-    }
+    }*/
 
     // compute partitions
     val partitions: List[InputPartition[InternalRow]] = new ArrayList()
