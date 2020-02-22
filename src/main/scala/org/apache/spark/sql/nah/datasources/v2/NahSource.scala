b2792285384bbdde617eb203c52071318d1a91bf
Daniel Rammer <hamersaw@protonmail.com>
2020-02-21 18:18:08 -0700

    cleaned NahSource.scala and NahSourceReader.scala files.

--- src/main/scala/org/apache/spark/sql/nah/datasources/v2/NahSource.scala
+++ src/main/scala/org/apache/spark/sql/nah/datasources/v2/NahSource.scala
@@ -6,10 +6,6 @@ import com.google.protobuf.ByteString
 
 import org.apache.hadoop.hdfs.protocol.proto.{HdfsProtos, ClientNamenodeProtocolProtos}
 
-import org.apache.hadoop.fs.{FileStatus, Path}
-
-import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.execution.datasources.csv.CSVFileFormat
 import org.apache.spark.sql.sources.DataSourceRegister
 import org.apache.spark.sql.sources.v2.{DataSourceOptions, DataSourceV2, ReadSupport}
 import org.apache.spark.sql.sources.v2.reader.DataSourceReader
@@ -34,7 +30,6 @@ class NahSource extends DataSourceV2 with ReadSupport with DataSourceRegister {
     // discover fileStatus for paths
     var storagePolicyId: Option[Int] = None
     var storagePolicy = ""
-    //var fileMap = Map[String, ListBuffer[FileStatus]]() // TODO - remove
 
     var blockMap = Map[Long, Long]()
     var blockLocations = Map[Long, ListBuffer[String]]()
@@ -118,32 +113,12 @@ class NahSource extends DataSourceV2 with ReadSupport with DataSourceRegister {
                   // populate datanodeMap with datanodeIdProto
                   if (!datanodeMap.contains(locationId)) {
                     datanodeMap += (locationId -> didProto)
-                    //println("added locationId '" + locationId + "'")
                   }
                 }
 
                 blockMap += (blockId -> blockLength)
                 blockLocations += (blockId -> locations)
-                //println("added block '" + blockId + "' locations:" + locations) 
               }
-
-              // TODO - remove
-              /*// initialize file
-              val filePath = new String(hfsProto.getPath.toByteArray)
-              val file = new FileStatus(hfsProto.getLength, false, 
-                hfsProto.getBlockReplication, 
-                hfsProto.getBlocksize, 
-                hfsProto.getModificationTime, new Path(filePath))
-
-              // add file to fileMap
-              fileMap.get(id) match {
-                case Some(files) => files += file
-                case None => {
-                  val files = new ListBuffer[FileStatus]()
-                  files += file
-                  fileMap += (id -> files)
-                }
-              }*/
             }
             case _ => {}
           }
@@ -261,145 +236,17 @@ class NahSource extends DataSourceV2 with ReadSupport with DataSourceRegister {
       dataSchema
     }
 
-    /*// compile dataSchema
-    val dataSchema: StructType =
-        options.get("inferSchema").orElse("false") match {
-      case "true" => {
-        // compile file list
-        val files = new ListBuffer[FileStatus]()
-        for ((_, f) <- fileMap) {
-          files ++= f
-        }
-
-        // inferSchema using Spark FileFormat
-        fileFormat match {
-          case "CsvPoint" => {
-            new CSVFileFormat().inferSchema(SparkSession.active,
-              Map("inferSchema" -> "true"), files).orNull
-          }
-          case "Wkt" => {
-            new CSVFileFormat().inferSchema(SparkSession.active,
-              Map("inferSchema" -> "true", "delimiter" -> "\t"), files).orNull
-          }
-        }
-      }
-      case _ => {
-        // TODO - get first block
-        // read first block
-        val (id, files) = fileMap.head
-        val file = files.head
-
-        // parse host, port
-        val idFields = id.split(":")
-        val (host, port) = (idFields(0), idFields(1).toInt)
-
-        // send GetFileInfo request
-        val gblRpcClient = new RpcClient(host, port, "ATLAS-SPARK",
-          "org.apache.hadoop.hdfs.protocol.ClientProtocol")
-        val gblRequest = ClientNamenodeProtocolProtos
-          .GetBlockLocationsRequestProto.newBuilder()
-            .setSrc(file.getPath.toString)
-            .setOffset(0)
-            .setLength(file.getLen)
-            .build
-
-        val gblIn = gblRpcClient.send("getBlockLocations", gblRequest)
-        val gblResponse = ClientNamenodeProtocolProtos
-          .GetBlockLocationsResponseProto.parseDelimitedFrom(gblIn)
-        gblIn.close
-        gblRpcClient.close
-
-        // process block locations
-        val lbsProto = gblResponse.getLocations
-        val lbProto = lbsProto.getBlocks(0)
-        
-        val blockId = lbProto.getB.getBlockId
-        val blockLength = lbProto.getB.getNumBytes
-
-        // parse locations
-        val blockData = new Array[Byte](blockLength.toInt)
-        breakable { for (diProto <- lbProto.getLocsList) {
-          val didProto = diProto.getId
-
-          val socket = new Socket(didProto.getIpAddr, didProto.getXferPort)
-          val dataOut = new DataOutputStream(socket.getOutputStream)
-          val dataIn = new DataInputStream(socket.getInputStream)
-
-          dataOut.writeShort(28); // protocol version
-          dataOut.write(83); // op - ReadBlockDirect
-          dataOut.write(0); // protobuf length
-          dataOut.writeLong(blockId);
-          dataOut.writeLong(0);
-          dataOut.writeLong(blockLength);
-
-          var offset = 0;
-          var bytesRead = 0;
-          while (offset < blockData.length) {
-            bytesRead = dataIn.read(blockData, offset,
-              blockData.length - offset)
-            offset += bytesRead
-          }
-
-          // send success indicator
-          dataOut.writeByte(0);
-
-          // close streams
-          dataIn.close
-          dataOut.close
-          socket.close
-
-          //  TODO - check for success
-          break
-        } }
-
-        // parse field count from block
-        var delimiterCount = 0
-        val inputStream = new ByteArrayInputStream(blockData)
-        val bufferedInputStream = new BufferedInputStream(inputStream)
-        val scanner = new Scanner(bufferedInputStream, "UTF-8")
-
-        breakable { while (scanner.hasNextLine) {
-          val line = scanner.nextLine
-          val count = line.count(_ == ',')
-
-          if (delimiterCount == 0) {
-            delimiterCount = count
-          } else if (delimiterCount == count) {
-            break
-          } else {
-            // TODO - throw error
-          }
-        }}
-
-        scanner.close()
-        bufferedInputStream.close()
-        inputStream.close()
-
-        // compile StructType
-        var dataSchema = new StructType()
-        for (i <- 0 to delimiterCount) {
-          dataSchema = dataSchema.add("_c" + i, DoubleType, true)
-        }
-
-        dataSchema
-      }
-    }*/
-
     // TODO - remove
     val schemaDuration = System.currentTimeMillis - schemaStart
     println("schemaDuration: " + schemaDuration)
 
     // return new NahSourceReader
-    val queryThreads = options.get("queryThreads").orElse("16").toInt
     val maxPartitionBytes =
       options.get("maxPartitionBytes").orElse("33554432").toLong
     val lookAheadBytes =
       options.get("lookAheadBytes").orElse("2048").toInt
 
-    //new NahSourceReader(fileMap, dataSchema, fileFormat,
-    //  formatFields, queryThreads, maxPartitionBytes, lookAheadBytes)
-    new NahSourceReader(blockMap, blockLocations, datanodeMap, 
-      dataSchema, fileFormat, formatFields, queryThreads,
-      maxPartitionBytes, lookAheadBytes)
+    new NahSourceReader(blockMap, blockLocations, datanodeMap, dataSchema, 
+      fileFormat, formatFields, maxPartitionBytes, lookAheadBytes)
   }
 }
--- src/main/scala/org/apache/spark/sql/nah/datasources/v2/NahSourceReader.scala
+++ src/main/scala/org/apache/spark/sql/nah/datasources/v2/NahSourceReader.scala
@@ -5,8 +5,7 @@ import io.blackpine.hdfs_comm.ipc.rpc.RpcClient
 import io.blackpine.nah.spark.sql.util.Converter
 import io.blackpine.nahfs.protocol.proto.NahFSProtos
 
-import org.apache.hadoop.hdfs.protocol.proto.{ClientNamenodeProtocolProtos, HdfsProtos}
-import org.apache.hadoop.fs.FileStatus
+import org.apache.hadoop.hdfs.protocol.proto.HdfsProtos
 
 import org.apache.spark.sql.catalyst.InternalRow
 import org.apache.spark.sql.sources.{GreaterThan, GreaterThanOrEqual, LessThan, LessThanOrEqual}
@@ -14,25 +13,18 @@ import org.apache.spark.sql.sources.Filter
 import org.apache.spark.sql.sources.v2.reader.{DataSourceReader, InputPartition, SupportsPushDownFilters, SupportsPushDownRequiredColumns}
 import org.apache.spark.sql.types.StructType
 
-import java.lang.Thread
 import java.util.{ArrayList, List}
-import java.util.concurrent.ArrayBlockingQueue
 
 import scala.collection.JavaConversions._
 import scala.collection.mutable.ListBuffer
 import scala.util.control.Breaks._
 
-//class NahSourceReader(fileMap: Map[String, Seq[FileStatus]],
-//    dataSchema: StructType, fileFormat: String,
-//    formatFields: Map[String, String], queryThreads: Int,
-//    maxPartitionBytes: Long, lookAheadBytes: Long) extends DataSourceReader
-//    with SupportsPushDownFilters with SupportsPushDownRequiredColumns {
 class NahSourceReader(blockMap: Map[Long, Long],
     blockLocations: Map[Long, ListBuffer[String]],
     datanodeMap: Map[String, HdfsProtos.DatanodeIDProto], 
     dataSchema: StructType, fileFormat: String,
-    formatFields: Map[String, String], queryThreads: Int,
-    maxPartitionBytes: Long, lookAheadBytes: Long) extends DataSourceReader
+    formatFields: Map[String, String], maxPartitionBytes: Long,
+    lookAheadBytes: Long) extends DataSourceReader
     with SupportsPushDownFilters with SupportsPushDownRequiredColumns {
   private final val FIRST_BIT = -9223372036854775808l;
   private final val INDEXED_MASK = -4294967296l;
@@ -137,7 +129,6 @@ class NahSourceReader(blockMap: Map[Long, Long],
     if (nahQuery.isEmpty) {
       processBlockMap = blockMap
     } else {
-      // TODO - perform only if nahQuery is populated
       // split blocks along namenode
       var blockSources = Map[String, ListBuffer[Long]]()
       for ((blockId, locations) <- blockLocations) {
@@ -188,117 +179,15 @@ class NahSourceReader(blockMap: Map[Long, Long],
 
     val partitionStart = System.currentTimeMillis // TODO - remove
 
-    /*// submit requests within threads
-    val inputQueue = new ArrayBlockingQueue[(String, Int, String, Long)](4096)
-    val outputQueue = new ArrayBlockingQueue[Any](128)
-
-    val threads: List[Thread] = new ArrayList()
-    for (i <- 1 to queryThreads) {
-      val thread = new Thread() {
-        override def run() {
-          breakable { while (true) {
-            // retrieve next file
-            val (ipAddress, port, filename, length) = inputQueue.take()
-            if (filename.isEmpty) {
-              break
-            }
-
-            // send GetFileInfo request
-            val gblRpcClient = new RpcClient(ipAddress, port, "ATLAS-SPARK",
-              "org.apache.hadoop.hdfs.protocol.ClientProtocol")
-            val gblRequest = ClientNamenodeProtocolProtos
-              .GetBlockLocationsRequestProto.newBuilder()
-                .setSrc(filename)
-                .setOffset(0)
-                .setLength(length)
-                .build
-
-            val gblIn = gblRpcClient.send("getBlockLocations", gblRequest)
-            val gblResponse = ClientNamenodeProtocolProtos
-              .GetBlockLocationsResponseProto.parseDelimitedFrom(gblIn)
-            gblIn.close
-            gblRpcClient.close
-
-            // process block locations
-            val lbsProto = gblResponse.getLocations
-            outputQueue.put(lbsProto)
-          } }
-
-          outputQueue.put("")
-        }
-      }
-
-      thread.start()
-      threads.append(thread)
-    }*/
-
-    /*// push paths down pipeline
-    for ((id, files) <- this.fileMap) {
-      // parse ipAddress, port
-      val idFields = id.split(":")
-      val (ipAddress, port) = (idFields(0), idFields(1).toInt)
-
-      for (file <- files) {
-        var path = file.getPath.toString
-        if (!nahQuery.isEmpty) {
-          path += ("+" + nahQuery)
-        }
-
-        inputQueue.put((ipAddress, port, path, file.getLen))
-      }
-    }
-
-    for (i <- 1 to queryThreads) {
-      inputQueue.put(("", 0, "", 0))
-    }*/
-
     // compute partitions
     val partitions: List[InputPartition[InternalRow]] = new ArrayList()
-
-    /*var poisonCount = 0
-    while (poisonCount < queryThreads) {
-      val result = outputQueue.take()
-      if (!result.isInstanceOf[HdfsProtos.LocatedBlocksProto]) {
-        poisonCount += 1
-      } else {
-        val lbsProto = result.asInstanceOf[HdfsProtos.LocatedBlocksProto]
-
-        for (lbProto <- lbsProto.getBlocksList) {
-          // parse block addresses
-          val locations = lbProto.getLocsList
-            .map(_.getId.getIpAddr).toArray
-          val ports = lbProto.getLocsList
-            .map(_.getId.getXferPort).toArray
-
-          var offset = 0l
-          val blockLength = lbProto.getB.getNumBytes
-          while (offset < blockLength) {
-            val length = scala.math.min(maxPartitionBytes,
-              blockLength - offset)
-            val lookAhead = scala.math.min(lookAheadBytes,
-              blockLength - (offset + length))
-
-            // initialize block partition
-            partitions += new NahPartition(dataSchema,
-              requiredSchema, lbProto.getB.getBlockId, offset,
-              length, offset == 0, lookAhead, locations, ports)
-
-            offset += length
-          }
-        }
-      }
-    }*/
-
-    // iterate over blocks
     for ((blockId, blockLength) <- processBlockMap) {
+      // compute locationBlockId -> if indexed only use first 32 bits
       var locationBlockId = blockId
       if ((blockId & FIRST_BIT) == FIRST_BIT) { // indexed block
         locationBlockId = locationBlockId & INDEXED_MASK
       }
 
-      // TODO - figure out how to get block locations
-      // when looking up location - check first bit (indexed)
-      //  if so lookup on first 32 bits - perform mask operation
       val locations = blockLocations.get(locationBlockId).orNull
         .map(datanodeMap.get(_).orNull.getIpAddr).toArray
       val ports = blockLocations.get(locationBlockId).orNull
